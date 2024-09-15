d7f5f3835779d3fedebb660c8fa8fd27a89dce25
AsinaRaoelison <raoelisonasina@gmail.com>
2024-09-15 16:35:42 +0300

    modification3

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -4,11 +4,11 @@ function Intro() {
   return (
     <div className="text-box" id='name-and-logo-text-box'>
       <h1 className='name-and-logo'>
-        Asina RAOELISON 
+        LUCA NYCKEES
       </h1>
       {/* <img src={my_logo} alt="My Logo" className="logo" /> */}
       <p>
-        test
+        Hey there, welcome to my homepage.
       </p>
       <Link to="about" smooth={true} duration={500} id="from-top-to-about">
         <a id="start-expand" className="material-symbols-outlined bouncing">
