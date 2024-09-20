cb466c283aae5d36155de9c144c563842e8ead6b
AsinaRaoelison <raoelisonasina@gmail.com>
2024-09-20 19:38:17 +0300

    tet

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -8,7 +8,7 @@ function Intro() {
       </h1>
       {/* <img src={my_logo} alt="My Logo" className="logo" /> */}
       <p>
-        Hey there, welcome to my homepage.
+        Hey there, welcome to my homepage test.
       </p>
       <Link to="about" smooth={true} duration={500} id="from-top-to-about">
         <a id="start-expand" className="material-symbols-outlined bouncing">
